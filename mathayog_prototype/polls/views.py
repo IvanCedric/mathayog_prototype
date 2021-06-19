51701c060e67c9c936af6b3f08e78f133162f8de
ivanc <ivan@inqbyte.io>
2021-06-19 11:02:38 +0800

    initial commit

--- mathayog_prototype/polls/views.py
+++ mathayog_prototype/polls/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+
+questions = [
+    {
+        "question_text": "What up?",
+        "pub_date": "2021-06-19"
+    },
+{
+        "question_text": "What up, dog?",
+        "pub_date": "2021-06-19"
+    },
+]
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {
+        "questions": Question.objects.all(),
+        "title": "Home"
+    }
+    return render(request, "home.html", context)
