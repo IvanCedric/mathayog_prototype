984d383ff465092c8d9fe2a61ec8048b9d55f784
unknown <korsie.ballesteros@gmail.com>
2021-06-25 08:24:13 +0800

    still no text input

--- mathayog_prototype/polls/static/js/main.js
+++ mathayog_prototype/polls/static/js/main.js
@@ -1,10 +1,7 @@
 const fill = document.querySelectorAll('.fill');
 const empties = document.querySelectorAll('.empty');
 
-document.getElementById( "done1" ).onclick = function() {
-  // img clicked
-  console.log("done1 clicked")
-};
+var state;
 // Fill listeners
 for (const fil of fill){
 
@@ -57,28 +54,91 @@ function dragDrop(e) {
   this.append(draggableElement)
 }
 
-/*var socket = new WebSocket('ws://localhost:8000/ws/some_url');
-
-  socket.onmmessage = function(e){
-  var data = JSON.parse(e.data);
-  console.log(data);
-  document.querySelector('#app').innerText = data.message;
-}*/
-
 /*code for area map of deck area*/
 $("#area1").on("click", function(e){
   e.preventDefault();
-  console.log('deck clicked')
-  /*
-   your code here
-  */
-});
+  console.log('deck clicked');
 
+  if (state == 0) {
+    //correct
+    $("#tp1-4").slideDown("slow");
+    setTimeout(function() {
+      $("#tp1-5").slideDown("slow");
+    }, 3000);
+    state = 1;
+  }
+  else if(state == 1){
+    $("#tp1-6").slideDown("slow");
+  }
+  
+  
+});
+/*code for area map of sail area*/
 $("#area2").on("click", function(e){
   e.preventDefault();
   console.log('sail clicked')
-  /*
-   your code here
-  */
+  if (state == 0) {
+    $("#tp1-3").slideDown("slow");
+  }
+  else if(state == 1){
+    //correct
+    $("#tp1-7").slideDown("slow");
+    setTimeout(function() {
+      $('#tutor-response-main').animate({width:'toggle'},350);
+      $("#bar-model").slideDown("slow");
+      $("#ship-deck").slideUp("slow");
+      $("#done1").slideUp("slow");
+      $('#tp1-3').slideUp("slow");
+      $('#tp1-4').slideUp("slow");
+      $('#tp1-5').slideUp("slow");
+      $('#tp1-6').slideUp("slow");
+      $('#tp1-7').slideUp("slow");
+    }, 2000);
+  }
 });
 
+// Student after finishing prelimary read of question
+$(document).ready(function(){
+  $("#done1").click(function(e){
+    state = 0;
+    $("#tp0-5").slideDown("slow");
+    setTimeout(function() {
+      $("#ship-deck").slideDown("slow");
+      $("#done1").slideUp("slow");
+      $('#tutor-response-main').animate({width:'toggle'},350);
+      $('#tp0-1').slideUp("slow");
+      $('#tp0-2').slideUp("slow");
+      $('#tp0-3').slideUp("slow");
+      $('#tp0-5').slideUp("slow");
+    }, 2000);
+    setTimeout(function() {
+      $('#tutor-response-main').slideDown("slow");
+    }, 3000);
+    setTimeout(function() {
+      $('#tp1-1').slideDown("slow");
+    }, 3500);
+    setTimeout(function() {
+      $('#tp1-2').slideDown("slow");
+    }, 5500);
+
+
+
+  
+  });
+});
+
+$(document).ready(function(){
+  setTimeout(function() {
+    console.log('first teacher prompt');
+    $("#tp0-1").slideDown("slow");
+    }, 2000);
+  setTimeout(function() {
+    console.log('second teacher prompt');
+    $("#tp0-2").slideDown("slow");
+    }, 4000);
+  setTimeout(function() {
+      console.log('third teacher prompt');
+      $("#tp0-3").slideDown("slow");
+      }, 6000); 
+  
+  });
